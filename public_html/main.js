6047c4c7c5f8694ecb43d459704a26db078ea699
touchard <touchard@Portable08>
2022-05-24 11:59:17 +0200

    Update READ.me

--- public_html/main.js
+++ public_html/main.js
@@ -1,29 +1,36 @@
-
-$.get('data.csv', function (csv) {
-    $('#container').highcharts({
-        data: {
-            csv: csv
-        },
-        title: {
-            text: "Hauteur de l'axe z"
-        },
-
-        subtitle: {
-            text: "Valeur de la vibration"
-        },
-        plotOptions: {
-            series: {
-                marker: {
-                    enabled: false
-                }
-            }
-        },
-        series: [{
-                lineWidth: 1,
-                type: 'line',
-                color: '#ff0000',
-                fillOpacity: 0.5
-            }]
-    });
+$(document).ready(function (){
+    $("#boutonConf").click(afficherGraph);
 });
 
+function afficherGraph()
+{
+    let fichiercsv = $('#btnFileUpload').val();
+    alert(fichiercsv);
+    $.get(fichiercsv, function (csv) {
+        $('#container').highcharts({
+            data: {
+                csv: csv
+            },
+            title: {
+                text: "Hauteur de l'axe z"
+            },
+
+            subtitle: {
+                text: "Valeur de la vibration"
+            },
+            plotOptions: {
+                series: {
+                    marker: {
+                        enabled: false
+                    }
+                }
+            },
+            series: [{
+                    lineWidth: 1,
+                    type: 'line',
+                    color: '#ff7f00',
+                    fillOpacity: 0.5
+                }]
+        });
+    });
+}
